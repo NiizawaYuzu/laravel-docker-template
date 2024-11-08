ee06e2a83496f18f02dec5b2c8511584f19b44a8
yuzuniizawa <yuzu.ringo0504@gmail.com>
2024-11-08 15:56:26 +0900

    'refactor:Modelインスタンスのリファクタリング'

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -6,38 +6,47 @@ use App\Todo;
 use Illuminate\Http\Request;
 
 class TodoController extends Controller
-{
+{   
+    private $todo; // 追記
+
+    public function __construct(Todo $todo)
+    {
+        $this->todo = $todo; // 追記
+    }
+    
     public function index()
     {
-        $todo = new Todo();
-        $todos = $todo->all();
+        // $todo = new Todo();
+        // $todos = $todo->all();
+        $todos = $this->todo->all();
 
         return view('todo.index', ['todos' => $todos]);
     }
 
     public function create()
     {
-        // TODO: 第1引数を指定
         return view('todo.create'); // 追記
     }
 
     public function store(Request $request) 
     {
         $inputs = $request->all(); // 変更
-        dd($inputs);
 
-        $todo = new Todo();
-        $todo->fill($inputs);
-        $todo->save();  
+        // $todo = new Todo();
+        // $todo->fill($inputs);
+        $this->todo->fill($inputs);
+        $this->todo->save(); 
 
         return redirect()->route('todo.index'); 
     }
 
     public function show($id)
     {
-        $model = new Todo();
+        $todo = $this->todo->find($id);
         $todo = $model->find($id);
 
         return view('todo.show', ['todo' => $todo]);
     }
+
+   
 }
