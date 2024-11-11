9fb13d3c0788e22afc849269d8825d60cec9c0bb
yuzuniizawa <yuzu.ringo0504@gmail.com>
2024-11-11 17:26:16 +0900

    'feat:編集画面にて内容を変更したデータを更新する機能追加'

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -31,6 +31,7 @@ class TodoController extends Controller
     public function store(Request $request) 
     {
         $inputs = $request->all(); // 変更
+        dd($inputs);
 
         // $todo = new Todo();
         // $todo->fill($inputs);
@@ -53,6 +54,16 @@ class TodoController extends Controller
         $todo = $this->todo->find($id);
         return view('todo.edit', ['todo' => $todo]);
 
+    }
+
+    public function update(Request $request, $id) // 第1引数: リクエスト情報の取得　第2引数: ルートパラメータの取得
+    {
+        // TODO: リクエストされた値を取得
+        $inputs = $request->all();//
+        $todo = $this->todo->find($id);//$todoの内容を再度定義
+        $todo->fill($inputs)->save();
+
+        return redirect()->route('todo.show', $todo->id);
     }
 
 }
